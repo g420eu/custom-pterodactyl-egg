cf6002995a826a8deb3d7d33c2fc450876496f6c
Ism1tha <xgalan@hotmail.com>
2025-02-10 17:22:38 +0100

    feat: update entrypoint script to replace startup variables and execute server command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /home/container
 
-echo "Hello G420EU"
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
